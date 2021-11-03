ea03b4337bd0c0ab2dfd897958b47e2938fb0ed2
Logan <loganvk18@gmail.com>
2021-11-03 18:58:35 +0530

    fixed few bugs and added loader

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-// $("#1").delay(1000).fadeOut(1000,()=>{
-   
-//     $("#2").delay(1000).show();
-//     $("#2").delay(1000).fadeOut(1000,()=>{
-//         $("#3").delay(1000).show();
-//         $("#3").delay(1000).fadeOut(1500,()=>{
-//             $("#loader").hide();
-//             $("#loaded").delay(1000).show();
+$("#1").delay(1000).fadeOut(1500,()=>{
+    $("#2").delay(1000).show();
+    $("#2").delay(1000).fadeOut(1000,()=>{
+        $("#3").delay(1000).show();
+        $("#3").delay(1000).fadeOut(1500,()=>{
+            $("#loader").hide();
+            $("#loaded").delay(1000).show();
             
-//         }); 
-//     });  
-//     });
+        }); 
+    });  
+    });
     
 function downloadpdf(){
     window.open("assets/resume.pdf", '_blank');
